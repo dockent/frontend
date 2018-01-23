9b180a460583eb7134e8a3afac589938b1371edd
Vladyslav Pozdnyakov <scary_donetskiy@live.com>
2018-01-23 22:37:51 +0200

    Define initialState for dashboard and show some data from API

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {bindActionCreators} from "redux";
 import * as DashboardActions from '../../actions/DashboardActions';
 import {connect} from "react-redux";
+import {Container, Grid, Icon, Statistic} from "semantic-ui-react";
 
 class Dashboard extends Component {
     componentWillMount() {
@@ -9,13 +10,36 @@ class Dashboard extends Component {
     }
 
     render() {
-        return (<p>dashboard</p>);
+        return (<Container>
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={3}>
+                        <Statistic>
+                            <Statistic.Value>{this.props.dashboard.Containers}</Statistic.Value>
+                            <Statistic.Label><Icon name='cube'/> Total containers</Statistic.Label>
+                        </Statistic>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <Statistic>
+                            <Statistic.Value>{this.props.dashboard.Images}</Statistic.Value>
+                            <Statistic.Label><Icon name='database'/> Total images</Statistic.Label>
+                        </Statistic>
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <Statistic>
+                            <Statistic.Value>{this.props.dashboard.ServerVersion}</Statistic.Value>
+                            <Statistic.Label><Icon name='fork'/>Server version</Statistic.Label>
+                        </Statistic>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Container>);
     }
 }
 
 function mapStateToProps(state) {
     return {
-        dashboard: state.dashboard.payload
+        dashboard: state.dashboard
     };
 }
 
--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -1,11 +1,24 @@
 import {DASHBOARD_REQUEST_SUCCESS} from "../constants/Dashboard";
 
-const initialState = {};
+const initialState = {
+    Containers: 0,
+    Images: 0,
+    ServerVersion: 0,
+    ContainersRunning: 0,
+    ContainersPaused: 0,
+    ContainersStopper: 0,
+    KernelVersion: 0,
+    OperatingSystem: 0,
+    OSType: 0,
+    Architecture: 0,
+    NCPU: 0,
+    MemTotal: 0
+};
 
 export default function dashboard(state = initialState, action) {
     switch (action.type) {
         case DASHBOARD_REQUEST_SUCCESS:
-            return {...state, payload: action.payload};
+            return {...state, ...action.payload};
         default:
             return state;
     }
