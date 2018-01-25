ed1a253792692ee6371f59c77ff7bd4475a24ede
Vladyslav Pozdnyakov <vladyslav.pozdnyakov@comodo.od.ua>
2018-01-25 17:39:18 +0200

    Show table data for containers list. Start creating action toolbar

--- src/components/Containers/index.js
+++ src/components/Containers/index.js
@@ -2,10 +2,22 @@ import React, {Component} from 'react';
 import {bindActionCreators} from "redux";
 import * as ContainersListActions from '../../actions/Containers/List';
 import {connect} from "react-redux";
-import {Breadcrumb, Container, Table} from "semantic-ui-react";
+import {Breadcrumb, Checkbox, Container, Header, Icon, Menu, Table} from "semantic-ui-react";
 import {Link} from "react-router-dom";
+import _ from 'lodash';
 
 class Containers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toolbarBtnState: {
+                play: false,
+                repeat: false,
+                stop: false,
+                remove: false
+            }
+        };
+    }
     componentWillMount() {
         this.props.actions.requestData();
     }
@@ -17,16 +29,31 @@ class Containers extends Component {
                 <Breadcrumb.Divider/>
                 <Breadcrumb.Section active>Containers</Breadcrumb.Section>
             </Breadcrumb>
-            <Table celled>
+            <Menu secondary>
+                <Menu.Item active={this.state.toolbarBtnState.play}><Icon name='play'/>Start</Menu.Item>
+                <Menu.Item active={this.state.toolbarBtnState.repeat}><Icon name='repeat'/>Restart</Menu.Item>
+                <Menu.Item active={this.state.toolbarBtnState.stop}><Icon name='stop'/>Stop</Menu.Item>
+                <Menu.Item active={this.state.toolbarBtnState.remove}><Icon name='trash'/>Remove</Menu.Item>
+            </Menu>
+            <Table celled striped>
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell>#</Table.HeaderCell>
                         <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Image</Table.HeaderCell>
                         <Table.HeaderCell>Status</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
+                    {_.map(this.props.list, (value, key) => (<Table.Row key={key}>
+                        <Table.Cell><Checkbox/></Table.Cell>
+                        <Table.Cell>
+                            <Header as='h4'>
+                                <Header.Content>{value.Names.join('; ')}</Header.Content>
+                                <Header.Subheader>{value.Image}</Header.Subheader>
+                            </Header>
+                        </Table.Cell>
+                        <Table.Cell>{value.Status}</Table.Cell>
+                    </Table.Row>))}
                 </Table.Body>
             </Table>
         </Container>);
